35e5356901cda425ea109b7f111506bfd2aa6b7b
Jacob Van De Weert <jacob@botsquare.com>
2013-07-20 15:14:03 -0700

    pull ClientHandler out of Server and into its own file

---
+++ ClientHandler.scala
@@ -0,0 +1,59 @@
+import java.io.{File, IOException, ObjectInputStream, ObjectOutputStream}
+import java.net.Socket
+import scala.collection.mutable.HashMap
+
+/* A ClientHandler is a Runnable object designed to handle all communications
+ * with a Client */
+class ClientHandler (client: Socket) (val hashes: HashMap[String, (Long, Array[Byte])]) extends Runnable {
+
+  val in = new ObjectInputStream(client.getInputStream)
+  val out = new ObjectOutputStream(client.getOutputStream)
+
+  def run : Unit = {
+    println("client connected")
+
+    // send client a list of file names and hashes
+    val fhList = hashes.toList.map(kv => (kv._1, kv._2._2))
+    val listMsg = FileListMessage(fhList)
+    out.writeObject(listMsg)
+
+    in.readObject match {
+      case FileRequest(files) => {
+
+        val fileList =
+          files.map { filename =>
+            val contents = Utils.readFile(new File(filename))
+            (filename, contents, hashes(filename)._2)
+          }
+        val msg = FileMessage(fileList)
+        out.writeObject(msg)
+
+        in.readObject match {
+          case Ack => ()
+          case _ => throw new IOException("Unknown or incorrect message received")
+        }
+      }
+      case _ => throw new IOException("Unknown or incorrect message received")
+    }
+
+    // main loop to listen for updated files
+    while (true) {
+      in.readObject match {
+        case FileMessage(updates) => {
+
+          updates.foreach { pbh =>
+            Utils.ensureDir(pbh._1)
+            val file = new File(pbh._1)
+            Utils.writeFile(file)(pbh._2)
+            // TODO(jacob) this is not safe with multiple clients
+            // TODO(jacob) verify correct hash
+            hashes.update(pbh._1, (file.lastModified, pbh._3))
+          }
+        }
+
+        case _ => throw new IOException("Unknown or incorrect message received")
+      }
+    }
+
+  }
+}
--- Server.scala
+++ Server.scala
@@ -1,63 +1,8 @@
-import java.io.{File, IOException, ObjectInputStream, ObjectOutputStream}
-import java.net.{ServerSocket, Socket}
+import java.io.File
+import java.net.ServerSocket
 import scala.collection.mutable.HashMap
 
 object Server {
-
-  /* A ClientHandler is a Runnable object designed to handle all communications
-   * with a Client */
-  class ClientHandler(val client: Socket) extends Runnable {
-
-    val in = new ObjectInputStream(client.getInputStream)
-    val out = new ObjectOutputStream(client.getOutputStream)
-
-    def run : Unit = {
-      println("client connected")
-
-      // send client a list of file names and hashes
-      val fhList = hashes.toList.map(kv => (kv._1, kv._2._2))
-      val listMsg = FileListMessage(fhList)
-      out.writeObject(listMsg)
-
-      in.readObject match {
-        case FileRequest(files) => {
-
-          val fileList =
-            files.map { filename =>
-              val contents = Utils.readFile(new File(filename))
-              (filename, contents, hashes(filename)._2)
-            }
-          val msg = FileMessage(fileList)
-          out.writeObject(msg)
-
-          in.readObject match {
-            case Ack => ()
-            case _ => throw new IOException("Unknown or incorrect message received")
-          }
-        }
-        case _ => throw new IOException("Unknown or incorrect message received")
-      }
-
-      while (true) {
-        in.readObject match {
-          case FileMessage(updates) => {
-
-            updates.foreach { pbh =>
-              Utils.ensureDir(pbh._1)
-              val file = new File(pbh._1)
-              Utils.writeFile(file)(pbh._2)
-              // TODO(jacob) this is not safe with multiple clients
-              // TODO(jacob) verify correct hash
-              hashes.update(pbh._1, (file.lastModified, pbh._3))
-            }
-          }
-
-          case _ => throw new IOException("Unknown or incorrect message received")
-        }
-      }
-
-    }
-  }
 
   // store file hashes of the most recent version
   // TODO(jacob) include version vectors at some point
@@ -86,7 +31,7 @@ object Server {
     println("listening for connections")
     while (true) {
       val client = serv.accept
-      val handler = new Thread(new ClientHandler(client))
+      val handler = new Thread(new ClientHandler(client)(hashes))
       handler.start
     }
 
