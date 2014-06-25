d5d9c79c0a791e375ccdd9701e70b68652703a46
Jacob Van De Weert <vandeweertj@gmail.com>
2014-06-24 22:31:48 -0400

    add serialVersionUID's to Serializable classes

--- util/FSObject.scala
+++ util/FSObject.scala
@@ -1,9 +1,12 @@
 package bopdrox.util
 
 /** A filesystem object is either a file or a directory */
-sealed trait FSObject {
+sealed trait FSObject extends Serializable {
   val path: FSPath
 }
 
-case class FSFile (val path: FSPath) extends FSObject
+@SerialVersionUID(0L)
 case class FSDirectory (val path: FSPath) extends FSObject
+
+@SerialVersionUID(1L)
+case class FSFile (val path: FSPath) extends FSObject
--- util/Message.scala
+++ util/Message.scala
@@ -7,91 +7,112 @@ sealed trait Message extends Serializable
 /** A message to be sent to acknowledge a received message when no other reply
   * is needed */
 // TODO(jacob) this is likely not necessary
+@SerialVersionUID(24601L)
 case object Ack extends Message
 
 
-sealed trait FLData {
+sealed trait FLData extends Serializable {
   val fsObj: FSObject
 }
+
+@SerialVersionUID(10L)
 case class FLDirectory (val dir: FSDirectory) extends FLData {
   val fsObj = dir
+  private val serialVersionUID = 10L
 }
+
+@SerialVersionUID(11L)
 case class FLFile (val file: FSFile, val hash: FileHash) extends FLData {
   val fsObj = file
+  private val serialVersionUID = 11L
 }
 
 /** Send a list of FSObjects with file hashes over the wire */
-case class FSListMessage (val fsList: List[FLData]) extends Message {
-  private val serialVersionUID = 20000024601L
-}
+@SerialVersionUID(20000024601L)
+case class FSListMessage (val fsList: List[FLData]) extends Message
 
 
-sealed trait FTData {
+sealed trait FTData extends Serializable {
   val fsObj: FSObject
   val oldFSObj: Option[FLData]
 }
+
+@SerialVersionUID(20L)
 case class FTDirectory (val dir: FSDirectory, val oldFSObj: Option[FLData])
     extends FTData {
   val fsObj = dir
+  private val serialVersionUID = 20L
 }
+
+@SerialVersionUID(21L)
 case class FTFile (val file: FSFile,
                    val contents: FileBytes,
                    val hash: FileHash,
                    val oldFSObj: Option[FLData])
     extends FTData {
   val fsObj = file
+  private val serialVersionUID = 21L
 }
 
 /** Transfer file contents over the wire */
-case class FSTransferMessage (val ftList: List[FTData])
-    extends Message {
-  private val serialVersionUID = 10000024601L
-}
+@SerialVersionUID(10000024601L)
+case class FSTransferMessage (val ftList: List[FTData]) extends Message
 
 
 /** Send a request for a list of FSObjects */
-case class FSRequest (val files: List[FSObject]) extends Message {
-  private val serialVersionUID = 30000024601L
-}
+@SerialVersionUID(30000024601L)
+case class FSRequest (val files: List[FSObject]) extends Message
 
 
-sealed trait Rejection {
+sealed trait Rejection extends Serializable {
   val fsObj: FSObject
 }
+
+@SerialVersionUID(30L)
 case class RejDirFile (val dir: FSDirectory,
                        val serverFile: FSFile,
                        val serverHash: FileHash)
     extends Rejection {
   val fsObj = dir
+  private val serialVersionUID = 30L
 }
+
+@SerialVersionUID(31L)
 case class RejFileDir (val file: FSFile, val hash: FileHash, val dir: FSDirectory)
     extends Rejection {
   val fsObj = file
+  private val serialVersionUID = 31L
 }
+
+@SerialVersionUID(32L)
 case class RejFileFile (val file: FSFile,
                         val hash: FileHash,
                         val serverHash: FileHash)
     extends Rejection {
   val fsObj = file
+  private val serialVersionUID = 32L
 }
+
+@SerialVersionUID(33L)
 case class RejDirNone (val dir: FSDirectory)
     extends Rejection {
   val fsObj = dir
+  private val serialVersionUID = 33L
 }
+
+@SerialVersionUID(34L)
 case class RejFileNone (val file: FSFile,
                         val hash: FileHash)
     extends Rejection {
   val fsObj = file
+  private val serialVersionUID = 34L
 }
 
 /** Notify a Client of rejected file updates */
-case class RejectUpdateMessage (val rejections: List[Rejection])
-    extends Message {
-  private val serialVersionUID = 50000024601L
-}
+@SerialVersionUID(50000024601L)
+case class RejectUpdateMessage (val rejections: List[Rejection]) extends Message
 
 
 /** Transfer a list of files that have been deleted */
-case class FSRemovedMessage (val removed: List[FLData]) extends Message {
-  private val serialVersionUID = 40000024601L
-}
+@SerialVersionUID(40000024601L)
+case class FSRemovedMessage (val removed: List[FLData]) extends Message
